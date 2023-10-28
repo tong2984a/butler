32fc7685d2ba6a5dc1cf3c3088fef450583a8f49
tong2984a <tong2984a@gmail.com>
2023-10-29 07:16:44 +0800

    init baseline to support family photos

--- install.sh
+++ install.sh
@@ -18,10 +18,12 @@ pip3 install sounddevice
 pip3 install soundfile
 pip3 install opencv-python
 pip3 install ffpyplayer
+pip3 install ffmpeg-python
+pip3 install pydub
 curl -L --retry 20 --retry-delay 2 -O https://github.com/tong2984a/butler/raw/main/test_halloween.mp4 
-curl https://gist.githubusercontent.com/tong2984a/15805276f9768012c2912af98e7975aa/raw/bf7628a6a733be854b798cf7fb86f6ebae6948de/butler_client.py --output butler_client.py
-curl https://gist.githubusercontent.com/tong2984a/6a0fb2280fe5bc8ddaae2036f916a28f/raw/c28b602309e0cdabb794f46f1e4c83075dd51cc3/butler_vosk.py --output butler_vosk.py
+curl -L --retry 20 --retry-delay 2 -O https://github.com/tong2984a/butler/raw/main/butler-client/butler_client.py
+curl -L --retry 20 --retry-delay 2 -O https://github.com/tong2984a/butler/raw/main/butler-client/butler_vosk.py
 cd ~/Desktop
-curl https://gist.githubusercontent.com/tong2984a/08feed94ac960d18eb74f2df69813c25/raw/fe17fa24fd3af1f60ea55d55bff1389db39f3a21/demo.sh --output demo.sh
+curl -L --retry 20 --retry-delay 2 -O https://github.com/tong2984a/butler/raw/main/demo.sh
 chmod +x demo.sh
 
