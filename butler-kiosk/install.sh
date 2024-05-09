68c1491452e0d71322a57503a63601e95d3f819f
tong2984a <tong2984a@gmail.com>
2024-05-09 19:03:47 +0800

    init

---
+++ butler-kiosk/install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+mkdir ~/.cache/vosk
+cd ~/.cache/vosk
+curl -L --retry 20 --retry-delay 2 -O https://github.com/tong2984a/butler/raw/main/butler-client/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+mkdir ~/work
+cd ~/work
+sudo apt -y update
+sudo apt -y install libasound2-dev
+sudo apt -y install portaudio19-dev
+sudo apt -y install libunistring-dev libaom-dev libdav1d-dev
+sudo apt -y install ffmpeg libavcodec-dev libavdevice-dev libavfilter-dev libavformat-dev libavutil-dev libswscale-dev libswresample-dev libpostproc-dev libsdl2-dev libsdl2-2.0-0 libsdl2-mixer-2.0-0 libsdl2-mixer-dev python3-dev
+sudo apt -y install vlc
+python3 -m venv venv
+source ./venv/bin/activate
+pip3 install simpleaudio
+pip3 install vosk
+pip3 install sounddevice
+pip3 install soundfile
+pip3 install opencv-python
+pip3 install ffpyplayer
+pip3 install ffmpeg-python
+pip3 install pydub
+pip3 install werkzeug
+pip3 install pyaudio
+pip3 install colorama
+pip3 install RPi.GPIO
+pip3 install python-vlc
+pip3 install langchain
+pip3 install faiss-cpu
+pip3 install sentence-transformers
+pip3 install streamlit
+
---
+++ butler-kiosk/kiosk_streamlit.py
@@ -0,0 +1,137 @@
+import faiss
+import pickle
+import json
+import streamlit as st
+
+from colorama import Fore, Back, Style
+import os
+import platform
+import time
+import sounddevice as sd
+import soundfile as sf
+import vlc
+import requests
+import RPi.GPIO as GPIO
+
+import logging
+import cv2
+import numpy as np
+from pydub import AudioSegment
+from pydub.playback import play
+import simpleaudio
+import pathlib
+import subprocess
+import ffmpeg
+from ffpyplayer.player import MediaPlayer
+from werkzeug.utils import secure_filename
+
+import butler_vosk
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+index = faiss.read_index("training.index")
+
+with open("faiss.pkl", "rb") as f:
+    store = pickle.load(f)
+
+store.index = index
+
+def clear_screen():
+    if platform.system() == 'Windows':
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def splash_screen(text=""):
+    print(Fore.WHITE + """這個機器,用來保存人類對生命的渴望和希冀""" + Style.RESET_ALL)
+    print(Fore.MAGENTA + """
+   @@@%                                                             +@@@                                                                              
+   @@@%                                                             +@@@                                                                              
+   @@@%                                                             +@@@                                                                              
+   @@@%                                                             +@@@                                                                              
+   @@@%                                                             +@@@                                                                              
+   @@@%           =@@@@@@@    @@@ @@@@ +@@=@@@@*@@@     @@@#        +@@@            @@@@@@@:                                                          
+   @@@%           @@@@@@@@@   @@@@@@@@ +@@@@@@@ @@@     @@@         +@@@           @@@@@@@@@                                                          
+   @@@%          @@@:   @@@   @@@@@:@  +@@@@-%# @@@#   :@@@         +@@@          %@@@   @@@%                                                         
+   @@@%           .@    @@@   @@@@     +@@@      @@@   @@@.         +@@@            +    *@@@                                                         
+   @@@%                @@@@   @@@@     +@@@      @@@   @@@          +@@@                *@@@@                                                         
+   @@@%            @@@@@@@@   @@@@     +@@@      @@@% :@@@          +@@@            %@@@@@@@@                                                         
+   @@@%          @@@@@@@@@@   @@@@     +@@@       @@@ @@@           +@@@           @@@@@@@@@@                                                         
+   @@@%          @@@    @@@   @@@%     +@@@       @@@ @@@           +@@@          @@@@   *@@@                                                         
+   @@@%         :@@@    @@@   @@@%     +@@@       -@@@@@@           +@@@          @@@    @@@@                                                         
+   @@@@@@@@@@@@  @@@   @@@@   @@@%     +@@@        @@@@@            +@@@@@@@@@@@  @@@@  #@@@@                                                         
+   @@@@@@@@@@@@  @@@@@@@@@@.  @@@%     +@@@        @@@@@            +@@@@@@@@@@@  @@@@@@@@@@@                                                         
+   @@@@@@@@@@@@   @@@@@  @@@  @@@%     +@@@         @@@-            +@@@@@@@@@@@   @@@@@% @@@                                                         
+                                                    @@@                                                                                               
+                                                   @@@@                                                                                               
+                                                @@@@@@                                                                                                
+                                                @@@@@@                                                                                                
+                                                @@@@                                                                                                  
+    """ + Style.RESET_ALL)
+
+    colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA]
+    for i, char in enumerate(text):
+        print(colors[i % len(colors)] + char, end="", flush=True)
+        time.sleep(0.05)
+    print(Style.RESET_ALL)
+
+def vlcVideo(file):
+    vlc_instance = vlc.Instance('--no-xlib -q > /dev/null 2>&1')
+    player = vlc_instance.media_player_new(file)
+    player.set_fullscreen(True)
+    player.play()
+    time.sleep(1.5)
+    duration = player.get_length() / 1000
+    time.sleep(duration)
+    player.stop()
+
+def runPrompt():
+    def onMessage(question):
+        docs_and_scores = store.similarity_search_with_score(question, k=1)
+        for i, doc in enumerate(docs_and_scores):
+            print("page content:", doc[0].page_content, "score:", doc[1])
+            json_object = json.loads(doc[0].page_content)
+            print("file:", json_object['File'])
+
+    while True:
+        question = input("Ask a question > ")
+        answer = onMessage(question)
+        print(f"Bot: {answer}")
+
+st.info('Sample Prompt 1 : What are the most rewarding aspects of your journey in the restaurant business that you hold close to your heart?')
+st.info('Sample Prompt 2 : Could you provide some background on your upbringing and the place that shaped you into the person you are today?')
+st.info('Sample Prompt 3 : Tell us about yourself.') 
+st.info("Sample Prompt 4 : Tell me about Larry La's journey in the restaurant business.")
+
+
+splash_screen()
+while True:
+    inp = "default"
+    filename = 'myrecording.wav'
+    is_pressed = GPIO.input(4)
+    try:
+        if is_pressed:
+            inp = butler_vosk.listen()
+            inp = inp.lower()
+            user_input = (json.loads(inp))["text"]
+            print(user_input)
+
+            st.chat_message("user").write(user_input)
+            with st.chat_message("assistant"):
+                docs_and_scores = store.similarity_search_with_score(user_input, k=1)
+                for i, doc in enumerate(docs_and_scores):
+                    print("page content:", doc[0].page_content, "score:", doc[1])
+                    json_object = json.loads(doc[0].page_content)
+                    mp4 = json_object['File']
+                    print("mp4:", mp4)
+                    subfolder = "mp4_files"
+                    file_path = os.path.join(subfolder, mp4)
+                    video_file = open(file_path, 'rb') #enter the filename with filepath
+                    video_bytes = video_file.read() #reading the file
+                    st.video(video_bytes) #displaying the video
+
+    except Exception as e:
+        print(e)
+        print('error - internet connection failed')
